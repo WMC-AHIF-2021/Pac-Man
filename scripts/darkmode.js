be9279c425e0fdf1f9d98e9855cc18da5eb6b5d5
elplak <elplak19@gmail.com>
2022-04-28 09:46:47 +0200

    headerImg-Update

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -14,6 +14,27 @@ function switchToDarkmode() {
     let modeImg = document.getElementById("mode");
     modeImg.src = "imgs/Dark-LightMode/darkmode.png";
 
+    try {
+        let imgHeaderHistory = document.getElementById("headerImgHistory");
+        imgHeaderHistory.src = "imgs/Header/Dark_PacmanHeaderHistory.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderGame = document.getElementById("headerImgGame");
+        imgHeaderGame.src = "imgs/Header/Dark_PacmanHeaderGame.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderRules = document.getElementById("headerImgRules");
+        imgHeaderRules.src = "imgs/Header/Dark_PacmanHeaderRules.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderIndex = document.getElementById("headerImgIndex");
+        imgHeaderIndex.src = "imgs/Header/Dark_PacmanHeaderIndex.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderCoa = document.getElementById("headerImgCoa");
+        imgHeaderCoa.src = "imgs/Header/Dark_PacmanHeaderCOA.jpg";
+    } catch(e) {}
+
     localStorage.isDarkmode = "true";
     modeImg.onclick = switchToLightmode;
 }
@@ -22,6 +43,27 @@ function switchToLightmode() {
     element.href = "css/stylesheet.css";
     let modeImg = document.getElementById("mode");
     modeImg.src = "imgs/Dark-LightMode/lightmode.png";
+
+    try {
+        let imgHeaderHistory = document.getElementById("headerImgHistory");
+        imgHeaderHistory.src = "imgs/Header/PacmanHeaderHistory.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderGame = document.getElementById("headerImgGame");
+        imgHeaderGame.src = "imgs/Header/PacmanHeaderGame.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderRules = document.getElementById("headerImgRules");
+        imgHeaderRules.src = "imgs/Header/PacmanHeaderRules.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderIndex = document.getElementById("headerImgIndex");
+        imgHeaderIndex.src = "imgs/Header/PacmanHeaderIndex.jpg";
+    } catch(e) {}
+    try {
+        let imgHeaderCoa = document.getElementById("headerImgCoa");
+        imgHeaderCoa.src = "imgs/Header/PacmanHeaderCOA.jpg";
+    } catch(e) {}
 
     let loadingScreen = document.getElementById("loadingScreen");
     loadingScreen.style.backgroundColor = "#ffffff";
