c26c8e8a53c4415fb8fe1b0a4c4111dbed814b14
elplak <elplak19@gmail.com>
2022-05-09 12:19:22 +0200

    htlLeonding-verlinkung

---
+++ build/Linking.js
@@ -0,0 +1,5 @@
+let element = document.getElementById("htlLeonding");
+element.addEventListener("click", function () {
+    element.innerHTML. = "whttps://www.htl-leonding.at/";
+});
+//# sourceMappingURL=Linking.js.map
