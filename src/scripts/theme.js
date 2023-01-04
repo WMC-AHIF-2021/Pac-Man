5befa74681d9138664f6f5a7e0fd0786f443c2c6
elplak <elplak19@gmail.com>
2023-01-04 14:43:59 +0100

    update theme.js + static part fix

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,6 +1,9 @@
 let page = window.location.pathname.split("/").pop().split(".")[0];
+
 let theme = document.getElementById("styles");
 let button = document.getElementById("theme");
+let htlLogo = document.getElementById("htlLogo");
+let loadingScreen = document.getElementById("loadingScreen");
 
 if (localStorage.darkMode == null) {
     enableDarkMode();
@@ -19,13 +22,34 @@ function enableLightMode() {
     let headerPath = "../img/headers/";
     switch (page) {
         case "index":
-            let header = document.getElementById("headerImgIndex");
-            header.src = headerPath + "index/headerIndex-light.png"
+            let headerIndex = document.getElementById("headerImgIndex");
+            headerIndex.src = headerPath + "index/headerIndex-light.png"
+            break;
+        case "rules":
+            let headerRules = document.getElementById("headerImgRules");
+            headerRules.src = headerPath + "rules/headerRules-light.jpg";
+            break;
+        case "history":
+            let headerHistory = document.getElementById("headerImgHistory");
+            headerHistory.src = headerPath + "history/headerHistory-light.jpg";
+            break;
+        case "changeOfAppearance":
+            let headerCOA = document.getElementById("headerImgCoa");
+            headerCOA.src = headerPath + "coa/headerCOA-light.jpg";
+            break;
+        case "game-singlePlayer":
+            let headerSinglePlayer = document.getElementById("headerImgGame");
+            headerSinglePlayer.src = headerPath + "game/headerGame-light.jpg";
+            break;
     }
-    // TODO otherHTMLFiles
 
     localStorage.darkMode = "false";
-    button.src = "../img/themeButtons/button-dark.png"
+
+    htlLogo.src = "../img/htlLogo/htlPacman-dark.png";
+
+    loadingScreen.style.backgroundColor = "white";
+
+    button.src = "../img/themeButtons/button-dark.png";
     button.onclick = enableDarkMode;
 }
 
@@ -36,11 +60,32 @@ function enableDarkMode() {
     switch (page) {
         case "index":
             let header = document.getElementById("headerImgIndex");
-            header.src = headerPath + "index/headerIndex-dark.png"
+            header.src = headerPath + "index/headerIndex-dark.png";
+            break;
+        case "rules":
+            let headerRules = document.getElementById("headerImgRules");
+            headerRules.src = headerPath + "rules/headerRules-dark.jpg";
+            break;
+        case "history":
+            let headerHistory = document.getElementById("headerImgHistory");
+            headerHistory.src = headerPath + "history/headerHistory-dark.jpg";
+            break;
+        case "changeOfAppearance":
+            let headerCOA = document.getElementById("headerImgCoa");
+            headerCOA.src = headerPath + "/coa/headerCOA-dark.jpg";
+            break;
+        case "game-singlePlayer":
+            let headerSinglePlayer = document.getElementById("headerImgGame");
+            headerSinglePlayer.src = headerPath + "game/headerGame-dark.jpg";
+            break;
     }
-    // TODO otherHTMLFiles
 
     localStorage.darkMode = "true";
-    button.src = "../img/themeButtons/button-light.png"
+
+    htlLogo.src = "../img/htlLogo/htlPacman-light.png";
+
+    loadingScreen.style.backgroundColor = "black";
+
+    button.src = "../img/themeButtons/button-light.png";
     button.onclick = enableLightMode;
 }
